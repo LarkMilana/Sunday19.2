e6aab7227cf29dc4d1eaadbb77d76774cef72cf5
LarkMilana <larkmilana@gmail.com>
2023-03-19 22:41:47 +0200

    TEST 2

--- src/test/java/Test2.java
+++ src/test/java/Test2.java
@@ -1,2 +1,12 @@
-package test.java;public class Test2 {
+//package test.java;
+
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.printf("Good night!");
+        System.out.printf("Good night!");
+        System.out.printf("Good night!");
+        System.out.printf("Good night!");
+        System.out.printf("Good night!");
+
+    }
 }
